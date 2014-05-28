1f727011b657d1bec1cbb327dfe57ab9020edea9
bill.anderson@rackspace.com <bill.anderson@rackspace.com>
2014-05-28 15:40:49 -0500

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='pelican-tutorial',
+    version='0.8',
+    packages=['tutorials'],
+    url='https://github.com/rackerlabs/pelican-tutorial',
+    license='AGPL',
+    author='Bill Anderson',
+    author_email='bill.anderson@rackspace.com',
+    description='A pelican tutorials plugin',
+    install_requires = ['pelican',]
+)
