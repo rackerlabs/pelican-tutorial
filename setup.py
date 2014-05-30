78d4be1f63c80c82fcb8561baedbbd3472c2716f
bill.anderson@rackspace.com <bill.anderson@rackspace.com>
2014-05-30 14:23:30 -0500

    correcting setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.8',
     packages=['pelican_tutorial'],
     url='https://github.com/rackerlabs/pelican-tutorial',
-    license='AGPL',
+    license='Apache 2.0',
     author='Bill Anderson',
     author_email='bill.anderson@rackspace.com',
     description='A pelican tutorials plugin',
